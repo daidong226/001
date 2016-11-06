e26aaccaa77f8322ea4b9764cdf4a9ab304207ab
daidong226 <daidong226@qq.com>
2016-11-06 14:27:10 +0800

    first

---
+++ pgui.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+from Tkinter import *
+
+root = Tk()
+root.title("July Music")   #标题
+root.geometry("300x200+700+200")
+entry = Entry()
+entry.pack()
+Button().pack()
+
+root.mainloop()
